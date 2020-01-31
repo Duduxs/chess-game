912da3bad31ffe0f05cb363c718f6e9b0c9a5312
Duduxs <duduxss3@gmail.com>
2020-01-31 06:42:50 -0300

    Error handling and custom exception

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -2,13 +2,16 @@ package boardgame;
 
 public class Board {
 
-	// Class board where they are size of board.
+	// Class board where they are elements of board
 	
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
 
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column!");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -18,30 +21,54 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
 	public Piece piece(int row, int column) {
+		if(!positionExists(row,column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
-	// Receive the piece on the insert position
+	/*Receive the piece on the insert position and test if
+     * alredy exists a piece in position
+	 */ 
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	/**
+	 * See if the part exists in a certain position, 
+	 *  making sure that the parts do not leave the range
+	 *   of the matrix
+	 */
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;
 	}
 
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,7 +26,7 @@ public class ChessMatch {
 	// Initialize the chess game, input the pieces on the board.
 	private void initialSetup() {
 		board.placePiece(new Rook(board,Color.WHITE), new Position(2,1));
-		board.placePiece(new King(board,Color.BLACK), new Position(0,4));
+		board.placePiece(new King(board,Color.BLACK), new Position(2,4));
 		board.placePiece(new King(board,Color.WHITE), new Position(7,4));
 	}
 }
