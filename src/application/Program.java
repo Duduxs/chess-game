85dd5abad363a0bdb94840eeace123192a3c5131
Duduxs <duduxss3@gmail.com>
2020-02-01 16:44:42 -0300

    Switching player

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 			// Treating errors
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
+				UI.printMatch(chessMatch);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
