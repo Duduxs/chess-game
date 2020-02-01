a163fbce5eab33622efb655ecfe19212f71c0646
Duduxs <duduxss3@gmail.com>
2020-02-01 18:06:13 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -62,6 +62,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 	
 	// Print BOARD columns
